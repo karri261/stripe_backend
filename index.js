f4e132dcbcde9cfb504b30cba5616b237db5fdc1
Duc Trien <karri.2605@gmail.com>
2025-05-02 11:30:21 +0700

    update

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const express = require('express');
 const Stripe = require('stripe');
 const cors = require('cors');
 
+const app = express(); 
+
 app.use(cors({
-    origin: '*', 
+    origin: '*',
     methods: ['GET', 'POST'],
     allowedHeaders: ['Content-Type']
 }));
 
-const app = express();
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY); // Sử dụng biến môi trường
 
 app.use(cors());
